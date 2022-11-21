6b834efd93acac903d53a0fb84abf41255421bc2
carlmenke <carlmenke13@gmail.com>
2022-11-21 15:26:41 -0800

    added function with O(n) run time.

---
+++ bigO.js
@@ -0,0 +1,13 @@
+const addUpTo = (n) =>{
+    let total = 0
+    for( let i = 1; i <= n; i++){
+        total++
+    }
+    return total
+}
+
+let t1 = performance.now()
+addUpTo(1000000000)
+let t2 = performance.now()
+
+console.log((t2-t1)/1000, " seconds")
