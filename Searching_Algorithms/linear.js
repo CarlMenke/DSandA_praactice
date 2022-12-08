4e02e5b8a13d57a2997871a1552eb90cab0e1425
carlmenke <carlmenke13@gmail.com>
2022-12-08 11:34:09 -0800

    created a linear search funciton

---
+++ Searching_Algorithms/linear.js
@@ -0,0 +1,9 @@
+// easiest way is to search an array is to look at each index and check it for true
+// this is N runtime
+
+const linearSearchFunction =(array, target)=> {
+    for( let i = 0; i < array.length; i++){
+        if(array[i] === target) return i
+    }
+    return -1
+}
